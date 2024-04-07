bfabbc0b986e71c07e1f018aab4b11c38ff6cba5
Sydney <github@shonx.net>
2024-04-06 23:13:36 -0400

    Update for deployment.

--- src/Program.cs
+++ src/Program.cs
@@ -1,8 +1,7 @@
 public class Program
 {
 
-    // public static readonly string BACKEND = "https://backend.docvault.shonx.net/";
-    public static readonly string BACKEND = "https://docvault-backend.graysky-3fc7894e.eastus.azurecontainerapps.io/";
+    public static readonly string BACKEND = "https://backend.docvault.shonx.net/";
     public static void Main(string[] args)
     {
         new WebHandler(args);
